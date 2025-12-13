a453c51d37bd6f766c2e72ea8bd94dbf166836c3
unknown <sohamsaha278@gmail.com>
2025-12-13 15:53:08 +0530

    added delete chat api

--- Backend/src/controller/ChatController.js
+++ Backend/src/controller/ChatController.js
@@ -1,52 +1,73 @@
 import { v7, validate } from "uuid";
 import Chat from "../models/Chat.js";
 
-export const createChat = async(req, res) => {
-    try {
-        const id = req.user.userID;
-        const chatId = v7();
+export const createChat = async (req, res) => {
+  try {
+    const id = req.user.userID;
+    const chatId = v7();
 
-        if(chatId !== null) {
-            const obj = {
-                userId: id,
-                conversationId: chatId,
-                lastMessage: "",
-            };
+    if (chatId !== null) {
+      const obj = {
+        userId: id,
+        conversationId: chatId,
+        lastMessage: "",
+      };
 
-            if(!validate(obj.conversationId)) {
-                return res.status(400).json({success: false, message: "UUID is not valid"});
-            } else {
-                const chat = await Chat.create(obj);
+      if (!validate(obj.conversationId)) {
+        return res
+          .status(400)
+          .json({ success: false, message: "UUID is not valid" });
+      } else {
+        const chat = await Chat.create(obj);
 
-                return res.status(201).json({
-                    success: true,
-                    message: "chat created successfully",
-                    chat,
-                })
-            }
-        }
-    } catch(err) {
-        console.log(err);
-        return res.status(400).json({
-            success: false,
-            message: "Error in creating chat",
-        })
+        return res.status(201).json({
+          success: true,
+          message: "chat created successfully",
+          chat,
+        });
+      }
     }
-}
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      success: false,
+      message: "Error in creating chat",
+    });
+  }
+};
 
-export const getChats = async(req, res) => {
-    try {
-        const userId = req.user.userID;
-        const chats = await Chat.find({userId}).sort({updatedAt: -1});
+export const getChats = async (req, res) => {
+  try {
+    const userId = req.user.userID;
+    const chats = await Chat.find({ userId }).sort({ updatedAt: -1 });
 
-        return res.status(200).json({
-            success: true,
-            chats
-        })
-    } catch(err) {
-        return res.status(400).json({
-            success: false,
-            message: err.message,
-        })
-    }
-}
+    return res.status(200).json({
+      success: true,
+      chats,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+export const deleteChat = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const data = await Chat.findOneAndDelete({ conversationId: id });
+    const del = await Chat.deleteMany({ conversationId: id });
+    return res.status(200).json({
+      success: true,
+      message: "Chat deleted successfully",
+      deletedChat: data,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      success: false,
+      message: "Server error while deleting chat",
+    });
+  }
+};
--- Backend/src/routes/ChatRoutes.js
+++ Backend/src/routes/ChatRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
-import { createChat, getChats } from "../controller/ChatController.js";
+import { createChat, deleteChat, getChats } from "../controller/ChatController.js";
 import { protect } from "../middleware/checkAuth.js";
 
 const router = express.Router();
 
 router.post("/create-chat", protect, createChat);
 router.get("/get-chats", protect, getChats);
+router.delete('/delete-chat/:id',protect, deleteChat);
 
 
 export default router;
